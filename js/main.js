ada586f8d831a7b322692c85cca67ed4f7bee07e
Sil Eggink <sil@niice.nl>
2025-09-04 10:27:42 +0200

    test

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-import './detect-scroll.js';
-import './form-validation.js';
-import './lenis-smoothscroll.js';
-import './update-year.js';
-import './cms-slider.js';
-import './swiper.js';
+// import './detect-scroll.js';
+// import './form-validation.js';
+// import './lenis-smoothscroll.js';
+// import './update-year.js';
+// import './cms-slider.js';
+// import './swiper.js';
