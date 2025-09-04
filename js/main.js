62c41efb92ab664551a5653acbc6002fcd5ec9db
Sil Eggink <sil@niice.nl>
2025-09-04 08:18:55 +0200

    add cms & swiper in main

--- js/main.js
+++ js/main.js
@@ -2,4 +2,5 @@ import './detect-scroll.js';
 import './form-validation.js';
 import './lenis-smoothscroll.js';
 import './update-year.js';
-import './cms-slider.js'
+import './cms-slider.js';
+import './swiper.js';
